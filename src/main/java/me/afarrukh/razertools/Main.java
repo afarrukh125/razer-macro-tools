e8fe6a7b23732f2703217ca4e006aaa0e1464f7f
Abdullah Farrukh <33581953+afarrukh125@users.noreply.github.com>
2025-12-26 12:20:40 +0000

    Add help command

--- src/main/java/me/afarrukh/razertools/Main.java
+++ src/main/java/me/afarrukh/razertools/Main.java
@@ -2,6 +2,7 @@ package me.afarrukh.razertools;
 
 import com.github.rvesse.airline.Cli;
 import com.github.rvesse.airline.builder.CliBuilder;
+import com.github.rvesse.airline.help.Help;
 import me.afarrukh.razertools.command.CollapseMouseMovementCommand;
 import me.afarrukh.razertools.command.FullCleanupCommand;
 import me.afarrukh.razertools.command.DoubleXCommand;
@@ -12,6 +13,7 @@ public class Main {
     public static void main(String[] args) {
         Cli<Runnable> cli = new CliBuilder<Runnable>("parser")
                 .withCommand(DoubleXCommand.class)
+                .withCommand(Help.class)
                 .withCommand(RemoveMouseMovementDelayCommand.class)
                 .withCommand(FullCleanupCommand.class)
                 .withCommand(CollapseMouseMovementCommand.class)
